795b6eccb021c1bcdf344f888bbdac5e6eb53ef6
liviv <viktorialvov@gmail.com>
2019-11-29 18:56:12 +0200

    29-11: CruisesPage, HomePge, ActionOnElements, abstractTest

---
+++ src/test/java/searchCruisesTest/SearchCruisesTest.java
@@ -0,0 +1,16 @@
+package searchCruisesTest;
+
+import abstractTest.AbstractTest;
+import org.junit.Test;
+
+public class SearchCruisesTest extends AbstractTest {
+
+    @Test
+
+
+
+    public void searchCruisesByNumberOfNights(){
+        // todo метод должен принимать константы 2-5, 6-8,9-11б 12+, и искать круизы с этим количеством ночей
+
+    }
+}
