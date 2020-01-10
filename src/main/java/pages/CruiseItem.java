445126be151bdb30da3e0fa8ac3bfc3c5a594399
liviv <viktorialvov@gmail.com>
2020-01-10 23:23:27 +0200

    corrected test 3  - remove  , from price

--- src/main/java/pages/CruiseItem.java
+++ src/main/java/pages/CruiseItem.java
@@ -5,19 +5,23 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import ru.yandex.qatools.htmlelements.element.HtmlElement;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class CruiseItem extends HtmlElement {
 
     Logger logger = Logger.getLogger(getClass());
 
     public String getCruiseName() {
         String value = cruiseName.getText().trim();
-        logger.info("Cruise name  is: " + value);
+        //logger.info("Cruise name  is: " + value);
         return value;
     }
 
     public String getPriceLabelStr() {
-        String value = price.getText().trim();
-        logger.info("Price is: " + value);
+        String value = price.getText().trim().replace(",", "");
+
+       // logger.info("Price is: " + value);
         return value;
     }
 
